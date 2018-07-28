28c4c338ef9bb431d0653a754a1a8882377c033c
Divya Chawla <divyachawla.1028@gmail.com>
2018-07-28 13:14:36 +0530

    databindings,ngtemplate,ifelse,structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  numbers = [1,2,3,4,5,6];
+  evenNumbers = [];
+  oddNumbers = []; 
+
+  ngOnInit() {
+  	for(let i = 0;i<this.numbers.length;i++) {
+  		if(this.numbers[i]%2==0) {
+  			this.evenNumbers.push(this.numbers[i]);
+  		}
+  		else{
+  			this.oddNumbers.push(this.numbers[i]);
+  		}
+  	}
+  }
 }
---
+++ src/app/component1/component1.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+})
+export class Component1Component implements OnInit {
+
+	onlyOdds:boolean = false;
+	@Output() status = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  buttonClick() {
+  	this.status.emit(!this.onlyOdds);
+  }
+
+}
